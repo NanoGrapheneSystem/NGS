08dc41bca5933056262d36399fabb44dfe716cda
NanoGrapheneSystem <37533364+NanoGrapheneSystem@users.noreply.github.com>
2018-04-10 03:17:16 +0800

    Update bancor.hpp

--- contracts/bancor/bancor.hpp
+++ contracts/bancor/bancor.hpp
@@ -1,21 +1,21 @@
 /**
  *  @file
- *  @copyright defined in eos/LICENSE.txt
+ *  @copyright defined in ngs/LICENSE.txt
  */
 #pragma once
 
-#include <eosiolib/eos.hpp>
-#include <eosiolib/token.hpp>
-#include <eosiolib/reflect.hpp>
-#include <eosiolib/generic_currency.hpp>
+#include <ngslib/eos.hpp>
+#include <ngslib/token.hpp>
+#include <ngslib/reflect.hpp>
+#include <ngslib/generic_currency.hpp>
 
 #include <bancor/converter.hpp>
 #include <currency/currency.hpp>
 
 namespace bancor {
-   typedef eosio::generic_currency< eosio::token<N(other),S(4,OTHER)> >  other_currency;
-   typedef eosio::generic_currency< eosio::token<N(bancor),S(4,RELAY)> > relay_currency;
-   typedef eosio::generic_currency< eosio::token<N(currency),S(4,CUR)> > cur_currency;
+   typedef ngs::generic_currency< ngs::token<N(other),S(4,OTHER)> >  other_currency;
+   typedef ngs::generic_currency< ngs::token<N(bancor),S(4,RELAY)> > relay_currency;
+   typedef ngs::generic_currency< ngs::token<N(currency),S(4,CUR)> > cur_currency;
 
    typedef converter<relay_currency, other_currency, cur_currency > example_converter;
 } /// bancor
